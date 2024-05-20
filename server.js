a92c9abdf1ff05230e2f9f1122a117219858e138
Osarugue Enehizena <nehirugue@gmail.com>
2024-05-20 15:01:22 +0100

    updated project both client and server side to use one localhost

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import required modules
 const http = require("node:http");
 const fs = require("fs");
+const path = require("path");
 
 // Set server hostname and port
 const hostname = "localhost";
@@ -10,43 +11,53 @@ const port = 4000;
 const server = http.createServer((req, res) => {
   // Set CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Handle POST requests to root URL
-  if (req.method === "POST" && req.url === "/") {
+  if (req.method === "GET" && req.url === "/") {
+    // Serve the index.html file
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else if (req.method === "POST" && req.url === "/") {
     let body = "";
-    // // Read request body
+
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
-    // Process request body when data ends
+
     req.on("end", () => {
       try {
-        // Parse request body as JSON
         const formData = JSON.parse(body);
 
         // Validate form fields
-        if (!formData?.firstName || !formData?.lastName) {
+        if (!formData.firstName || !formData.lastName) {
           throw new Error("First name and last name are required.");
         }
 
         if (
           !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-            formData?.email
+            formData.email
           )
         ) {
           throw new Error("Invalid email address.");
         }
-        if (formData?.mobileNumber?.length !== 11) {
+
+        if (formData.mobileNumber.length !== 11) {
           throw new Error("Mobile number must be 11 characters.");
         }
-        if (!formData?.gender) {
+
+        if (!formData.gender) {
           throw new Error("Gender is required.");
         }
 
-        if (!["MALE", "FEMALE"].includes(formData?.gender)) {
+        if (!["MALE", "FEMALE"].includes(formData.gender)) {
           throw new Error("Gender must be one of the following (MALE, FEMALE)");
         }
 
@@ -56,24 +67,20 @@ const server = http.createServer((req, res) => {
           JSON.stringify(formData, null, 2),
           (err) => {
             if (err) {
-              // Respond with Internal Server Error if writing fails
               res.writeHead(500, { "Content-Type": "text/plain" });
               res.end("Internal Server Error");
             } else {
-              // Respond with success message if writing succeeds
               res.writeHead(200, { "Content-Type": "application/json" });
               res.end(JSON.stringify({ message: "User created successfully" }));
             }
           }
         );
       } catch (error) {
-        // Respond with Bad Request if any error occurs during processing
         res.writeHead(400, { "Content-Type": "text/plain" });
         res.end(error.message);
       }
     });
   } else {
-    // Respond with Not Found for requests other than POST to root URL
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
   }
